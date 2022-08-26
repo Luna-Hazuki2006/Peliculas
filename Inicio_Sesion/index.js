26bd4cbf97f061196ce251f2c48e106b9e193726
Luna Hazuki <LunaHazuki2006@outlook.com>
2022-08-26 08:10:19 -0400

    con un iniciar sesión bonito

--- Inicio_Sesion/index.js
+++ Inicio_Sesion/index.js
@@ -1,7 +1,40 @@
-const labels = document.querySelectorAll('.form-control label')
-labels.forEach(label => {
-    label.innerHTML = label.innerText
-    .split('')
-    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
-    .join('')
-})
+// const labels = document.querySelectorAll('.form-control label')
+// labels.forEach(label => {
+//     label.innerHTML = label.innerText
+//     .split('')
+//     .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+//     .join('')
+// })
+async function getJSON(path) {
+    const response = await fetch(path)
+    return await response.json()
+}
+// por si acaso
+const dar_data = async (url, method = "GET", body = null) => {
+    try {
+        // const token = localStorage.getItem("token")
+        // !token && ( window.location.href = "/" )
+        console.log("va bien");
+        const response = await fetch(url, {
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Authorization': `Bearer ${token}`
+            }, 
+            method: method,
+            body: body
+        })
+        console.log(response);
+        console.log("por aquí también");
+        // transformar a JSON
+        const data = await response.json()
+        console.log("aun nada malo");
+        console.log(data.data);
+        return {
+            data, response
+        }
+    } catch (error) {
+        console.error(error)
+        console.log("hubo un error");
+    }
+}
+// también por si acaso
