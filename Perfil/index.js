2d06d5541c03b5346254b8e9af623ae0217e30dd
Luna Hazuki <LunaHazuki2006@outlook.com>
2022-08-30 16:30:26 -0400

    perfil completamente funcional

--- Perfil/index.js
+++ Perfil/index.js
@@ -38,6 +38,7 @@ const correo = document.getElementById("correo")
 const nombres = document.getElementById("nombres")
 const apellidos = document.getElementById("apellidos")
 const cedula = document.getElementById("cedula")
+const fecha = document.getElementById("nacimiento")
 const direccion = document.getElementById("direccion")
 
 const datos_iniciales = async () => {
@@ -49,6 +50,7 @@ const datos_iniciales = async () => {
     apellidos.innerText = "Apellidos: " + info.data.data.lastName
     cedula.innerText = "Cédula: " + info.data.data.id
     direccion.innerText = "Dirección: " + info.data.data.address
+    fecha.innerText = "Fecha de nacimiento: " + info.data.data.birthday
 }
 
 datos_iniciales()
@@ -75,10 +77,65 @@ vistaDependiente(0, false)
 vistaDependiente(1, false)
 vistaDependiente(2, false)
 
-modificarPerfil?.addEventListener("click", () => {
+modificarPerfil?.addEventListener("click", async () => {
     vistaDependiente(2, true)
     vistaDependiente(0, false)
     vistaDependiente(1, false)
+    const nombresCambio = document.getElementById("nombres-cambio")
+    const apellidosCambio = document.getElementById("apellidos-cambio")
+    const cedulaCambio = document.getElementById("cedula-cambio")
+    const nacimientoCambio = document.getElementById("nacimiento-cambio")
+    const direccionCambio = document.getElementById("direccion-cambio")
+    const cambiarla = document.getElementById("cambiar-perfil")
+    const info = await dar_data("https://vg-cine-server.herokuapp.com/profile")
+    nombresCambio.value = info.data.data.firstName
+    apellidosCambio.value = info.data.data.lastName
+    cedulaCambio.value = info.data.data.id
+    nacimientoCambio.value = info.data.data.birthday
+    direccionCambio.value = info.data.data.address
+    cambiarla?.addEventListener("submit", async (event) => {
+        event.preventDefault()
+        if (nombresCambio.value === "" || apellidosCambio.value === "" || 
+                cedulaCambio.value === "" || nacimientoCambio.value === "NaN/NaN/NaN" || 
+                direccionCambio.value === "") {
+            Swal.fire(
+                '¡Oh no!',
+                'No se puede cambiar el perfil si todos los campos no están llenos',
+                'error'
+            )
+            return
+        }
+        const datos = {
+            "firstName": nombresCambio.value,
+            "lastName": apellidosCambio.value,
+            "id": cedulaCambio.value,
+            "address": direccionCambio.value,
+            "birthday": nacimientoCambio.value
+        }
+        const data = await dar_data("https://vg-cine-server.herokuapp.com/edit-profile", "PUT", JSON.stringify(datos))
+        if (data.response.status === 200) {
+            Swal.fire(
+                '¡Lo lograste!',
+                'El perfil ha sido cambiado :D',
+                'success'
+            )
+        } else {
+            Swal.fire(
+                '¡Oh no!',
+                data.data.error,
+                'error'
+            )
+        }
+        nombresCambio.value = ""
+        apellidosCambio.value = ""
+        cedulaCambio.value = ""
+        direccionCambio.value = ""
+        nacimientoCambio.value = ""
+        datos_iniciales()
+        vistaDependiente(0, false)
+        vistaDependiente(1, false)
+        vistaDependiente(2, false)
+    })
 })
 
 cambiarContraseña?.addEventListener("click", () => {
@@ -86,16 +143,29 @@ cambiarContraseña?.addEventListener("click", () => {
     vistaDependiente(1, false)
     vistaDependiente(2, false)
     const cambiarla = document.getElementById("cambiar-contraseña")
-    cambiarla.addEventListener("submit", async (event) => {
+    cambiarla?.addEventListener("submit", async (event) => {
         event.preventDefault()
         const primera = document.getElementById("primera")
         const segunda = document.getElementById("segunda")
-        if (primera.value === segunda.value) {
+        if (primera.value === segunda.value && primera.value !== "" && segunda.value !== "") {
             const info = {
                 "password": primera.value,
                 "confirmPassword": segunda.value
             }
             const data = await dar_data("https://vg-cine-server.herokuapp.com/change-password", "PUT", JSON.stringify(info))
+            if (data.response.status === 200) {
+                Swal.fire(
+                    '¡Lo lograste!',
+                    'La contraseña ha sido cambiada :D',
+                    'success'
+                )
+            } else {
+                Swal.fire(
+                    '¡Oh no!',
+                    data.data.error,
+                    'error'
+                )
+            }
         } else {
             Swal.fire(
                 '¡On no!',
@@ -103,6 +173,11 @@ cambiarContraseña?.addEventListener("click", () => {
                 'error'
             )
         }
+        primera.value = ""
+        segunda.value = ""
+        vistaDependiente(0, false)
+        vistaDependiente(1, false)
+        vistaDependiente(2, false)
     })
 })
 
@@ -113,7 +188,20 @@ verCompras?.addEventListener("click", () => {
 })
 
 cerrarSesion?.addEventListener("click", () => {
-    localStorage.removeItem("token")
-    console.log("cerrar sesión");
-    window.location.href = "/"
+    Swal.fire({
+        title: '¿Realmente quieres cerrar sesión?',
+        text: "¡No podrás acceder a tu perfil!",
+        icon: 'question',
+        showCancelButton: true,
+        // confirmButtonColor: '#3085d6',
+        // cancelButtonColor: '#d33',
+        confirmButtonText: '¡Si, quiero cerrar sesión!', 
+        cancelButtonText: 'No'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            localStorage.removeItem("token")
+            console.log("cerrar sesión");
+            window.location.href = "/"
+        }
+    })
 })
