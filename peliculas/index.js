8a49078944bf1dccbc50621ce3bf4da413be9539
Luna Hazuki <LunaHazuki2006@outlook.com>
2022-09-01 09:40:47 -0400

    ya se ven todas las peliculas

--- peliculas/index.js
+++ peliculas/index.js
@@ -32,3 +32,35 @@ const dar_data = async (url, method = "GET", body = null) => {
 }
 // también por si acaso
 
+const verdad = (localStorage.getItem("token")) ? true : false
+
+const iniciar = document.getElementById("iniciar")
+const registrar = document.getElementById("registrar")
+const perfil = document.getElementById("perfil")
+
+const verificar = () => {
+    iniciar.style.display = (!verdad) ? "display" : "none"
+    registrar.style.display = (!verdad) ? "display" : "none"
+    perfil.style.display = (verdad) ? "display" : "none"
+}
+
+verificar()
+
+const listar = async () => {
+    const lista = document.getElementById("lista")
+    const a = await dar_data("https://vg-cine-server.herokuapp.com/movies?all=true")
+    console.log(a);
+    const data = a.data
+    lista.innerHTML = ""
+    data.data.forEach((element) => {
+        lista.innerHTML += `
+        <div>
+            <a href="../pelicula/index.html?id=${element.id}">
+                <img class="peliculas" src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt="${element.title}">
+            </a>
+        </div>
+        `
+    });
+}
+
+listar()
