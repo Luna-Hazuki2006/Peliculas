bc038e96cb11d6c1038c094d9405b6714d48e1a1
Luna Hazuki <LunaHazuki2006@outlook.com>
2022-09-05 10:28:02 -0400

    con todos los cines

--- cines/index.js
+++ cines/index.js
@@ -1,3 +1,26 @@
+const cines = [
+    ["https://www.cinex.com.ve/assets/images/cinemas/xtln.jpg.pagespeed.ic.KmV8PiXYqp.webp", "El Tolón"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/xrec.jpg.pagespeed.ic.hxfdg8DP9U.webp", "El Recreo"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/xsbc.jpg.pagespeed.ic.hDCWf-NjWy.webp", "El Sambil"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/xcsi.jpg.pagespeed.ic.oajBJ41KSN.webp", "San Ignacio"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/xipc.jpg.pagespeed.ic.S921zps9QZ.webp", "Los Próceres"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/xmpp.jpg.pagespeed.ic.SGjOfRR1s8.webp", "Multiplaza Paraíso"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/xman.jpg.pagespeed.ic.vwnnPYU4sx.webp", "Los Manzanares"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/xhat.jpg.pagespeed.ic.e76e-ov-K8.webp", "Paseo El Hatillo"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/xbvg.jpg.pagespeed.ic.FYrbKCtHbX.webp", "Buenaventura"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/xpca.jpg.pagespeed.ic.y4DzknNqDS.webp", "Parque Costazul"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/xmtb.jpg.pagespeed.ic.iypFHCWSe8.webp", "El Metrópolis Barquisimeto"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/xsbp.jpg.pagespeed.ic.8hiD8xaK7E.webp", "Sambil Paraguaná"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/x38.jpg.pagespeed.ic.AhSYIjwE6L.webp", "Galerías Mall"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/x39.jpg.pagespeed.ic.iXrHUtPIld.webp", "Costa Mall"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/x35.jpg.pagespeed.ic.AhSYIjwE6L.webp", "Marina Plaza"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/x30.jpg.pagespeed.ic.eb-Khms5Nc.webp", "Plaza Mayor"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/x32.jpg.pagespeed.ic.fEPy-RH-hz.webp", "Cima Plaza"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/x36.jpg.pagespeed.ic.ZZ1tqYXl4T.webp", "Las Virtudes"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/x37.jpg.pagespeed.ic.JwvpDM4LcA.webp", "Doral Plaza"], 
+    ["https://www.cinex.com.ve/assets/images/cinemas/x34.jpg.pagespeed.ic.AhSYIjwE6L.webp", "Valera"]
+]
+
 async function getJSON(path) {
     const response = await fetch(path)
     return await response.json()
@@ -45,3 +68,19 @@ const verificar = () => {
 }
 
 verificar()
+
+const init = () => {
+    const lista = document.getElementById("lista")
+    lista.innerHTML = ""
+    cines.forEach((element) => {
+        lista.innerHTML += `
+        <div>
+            <a href="">
+                <img class="cines" src="${element[0]}" alt="${element[1]}">
+            </a>
+        </div>
+        `
+    })
+}
+
+init()
